8850b4359668e84e9f32fce599aab3de7fbe46bc
ViktorT777CT <p_viktor91@mail.ru>
2023-03-22 17:55:52 +0600

    finall

---
+++ js/js.js
@@ -0,0 +1,14 @@
+ItcSubmitForm.getOrCreateInstance('form');
+
+document.addEventListener('itc.successSendForm', (e) => {
+    const el = e.target.closest('.form-container').querySelector('.form-success');
+    el.classList.remove('form-success_hide');
+});
+
+// при клике на .form-success__btn
+document.querySelector('.form-success__btn').addEventListener('click', (e) => {
+    const el = e.target.closest('.form-container').querySelector('form');
+    const form = ItcSubmitForm.getOrCreateInstance(el);
+    form.reset();
+    e.target.closest('.form-container').querySelector('.form-success').classList.add('form-success_hide');
+});
